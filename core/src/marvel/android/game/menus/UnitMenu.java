1d9f0d0b5857d9c99603d2454d3cba65526276a8
MrMarvel [S30] <seregakkkk999@yandex.ru>
2019-05-19 19:28:11 +0300

    bug

--- core/src/marvel/android/game/menus/UnitMenu.java
+++ core/src/marvel/android/game/menus/UnitMenu.java
@@ -1,16 +1,3 @@
 package marvel.android.game.menus;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 public class UnitMenu {
-    public UnitMenu() {
-
-    }
-<<<<<<< HEAD
-=======
-
-    public void draw(SpriteBatch) {
-
-    }
->>>>>>> 24666ab0b7406b1bd80650ddfd59ef66f0a72d48
 }
